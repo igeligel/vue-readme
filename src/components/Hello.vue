38c9aa2b6016d011f6b79399a000eaff06a9b3fb
Kevin Peters <git.igeligel@googlemail.com>
2017-03-23 22:54:15 +0100

    feat(hello)
    add panel layout

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="pure-g">
-    <div class="pure-u-1 pure-u-md-6-24 panel settings-panel">
-      <input type="text" name="repository-name" placeholder="Repository Name" />
+    <div class="pure-u-1 pure-u-md-1-6 panel menu-panel">
+      <!--<input type="text" name="repository-name" placeholder="Repository Name" />
       <input type="text" name="repository-sponsors" placeholder="Sponsors" />
       <textarea rows="4" cols="50" placeholder="Description"></textarea>
-      <input type="text" name="repository-dependency" placeholder="Dependency" />
+      <input type="text" name="repository-dependency" placeholder="Dependency" />-->
     </div>
-    <div class="pure-u-1 pure-u-md-18-24 panel readme-panel">
+    <div class="pure-u-1 pure-u-md-2-3 panel readme-panel">
+
+    </div>
+    <div class="pure-u-1 pure-u-md-1-6 panel settings-panel">
 
     </div>
   </div>
@@ -19,7 +22,14 @@ export default {
 </script>
 
 <style scoped>
+.menu-panel {
+  height: calc(100vh - 126px);
+}
+
+* {
+  border: 1px solid red;
+}
+
 .readme-panel {
-  max-width: 980px;
 }
 </style>
