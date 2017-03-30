19534787bf2af888308c973e590cc588fe38c432
Kevin Peters <git.igeligel@googlemail.com>
2017-03-30 20:08:21 +0100

    chore(project)
    add build step

--- build/publish.js
+++ build/publish.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), function(err) {
+ghpages.publish(path.join(__dirname, '../dist'), function(err) {
   console.log('Error publishing to gh-pages branch...');
   console.log(err);
 });
