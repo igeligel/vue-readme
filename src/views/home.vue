c3a2891e9ede6d73a3c96fa3285e101e620d7945
Kevin Peters <git.igeligel@googlemail.com>
2017-04-02 23:35:32 +0200

    refactor(home)
    use button component

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,9 @@
   <div class="pure-g">
     <div class="pure-u-1 pure-u-md-1-5 panel menu-panel" style="margin-top: 40px;">
       <div style="padding-left: 20px; padding-right: 20px;">
-        <button class="add-button">Create README.md</button>
+        <vue-r-button
+          :label="'Create README.md'">
+        </vue-r-button>
       </div>
       <div>
         <div class="menu-item"><span class="menu-item-icon">X</span>Editor</div>
